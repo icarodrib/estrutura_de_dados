e7aa5b9cbde1742efe957240c6cee87288f3307b
icarodrib <icarodrib@gmail.com>
2023-08-31 12:56:47 -0300

    Modificação para função + dever de casa

--- aula4.py
+++ aula4.py
@@ -1,24 +1,32 @@
+# Função para a ordenação das cartas
+def ordenar():
+    for i in range(qtde):
+        for j in range(qtde-1):
+            if cartas[j] > cartas[j+1]:
+                cartas[j], cartas[j+1]=cartas[j+1], cartas[j]
+
+# ---Início do programa----------------------------------------------------------------------------------------------------------------------------------
 cartas = []
 """ maxLenghtList = 3 """
-# entrada de dados da quantidade
+# Entrada da quantidade de cartas
 qtde = int(input("Declare quantas cartas pretende digitar: "))
 
-# entrada de dados na lista
+# Entrada de dados na lista
 for i in range(qtde):
     cartas.append(int(input(f"Digite a carta Nº{i+1}: ")))
 
-# imprimi as cartas digitadas
+# Imprimi as cartas digitadas
 print("\nCartas na ordem de entrada:")
 for i in range(qtde):
     print(f"A carta Nº{i+1} é:",cartas[i])
 
-# ordenação das cartas
-for i in range(qtde):
-    for j in range(qtde-1):
-        if cartas[j] > cartas[j+1]:
-            cartas[j], cartas[j+1]=cartas[j+1], cartas[j]
+# Chamada de função para realizar a ordenação
+ordenar()
 
-# imprime as cartas ordenadamente
-print("\nCartas ordem crescente:")
+# Imprime as cartas ordenadamente
+print("\nCartas na ordem crescente:")
 for i in range(qtde):
     print(f"A carta Nº{i+1} é:",cartas[i])
+
+# PARA FAZER EM CASA:
+# Fazer uma função para ordenar e outra para o usuario digitar os valores, fazer isso em uma biblioteca.
